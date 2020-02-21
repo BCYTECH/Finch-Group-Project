5ba6e5285dd46427641d33407d12a80667a96227
BCYTECH <41265567+BCYTECH@users.noreply.github.com>
2020-02-21 16:42:24 +0000

    finch detect light (with timer)

--- src/main.java
+++ src/main.java
@@ -1,9 +1,20 @@
+import edu.cmu.ri.createlab.terk.robot.finch.Finch;
 
-public class main {
+public class main 
+{
+	static Finch myfi = new Finch();
+	final private static int testtime = 10000;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main(String[] args)
+	{
+	int lileft = myfi.getLeftLightSensor();
+	int liright = myfi.getRightLightSensor();	
+		
+		System.out.println("\n"+"Running: ");
+		long before = System.currentTimeMillis();
+		while(System.currentTimeMillis() - before < testtime)
+		{
+			System.out.println(myfi.getLeftLightSensor() + " " + myfi.getRightLightSensor());
+		}
+	}	
 }
