99ec4035d0ba997cd45febf128128fe193256dc1
BCYTECH <41265567+BCYTECH@users.noreply.github.com>
2020-03-09 22:13:17 +0000

    New update: wheel speed, torch capture...etc

--- src/main.java
+++ src/main.java
@@ -7,15 +7,16 @@ public class main
 
 	public static void main(String[] args)
 	{
+		//139
 		int atmosphere = (myfi.getLeftLightSensor()+myfi.getRightLightSensor()/2);			//average atmosphere light reading
-		System.out.println("Ready for room light capturing...");
+		System.out.println("Ready for torch light capturing...");
 		JOptionPane.showMessageDialog(null,"Click OK to start running");
 
 		int left_light = myfi.getLeftLightSensor();
 		int right_light = myfi.getRightLightSensor();
 
 		//START
-		System.out.println("Reading ambient lighting: "+left_light+" "+right_light);
+		System.out.println("Reading ambient torch lighting: "+left_light+" "+right_light);
 		System.out.println("<<<RUNNING>>>");
 	
 		// 		add another method here
@@ -31,55 +32,54 @@ public class main
 				}
 				else 
 				{
-					if (myfi.getLeftLightSensor() + myfi.getRightLightSensor() > atmosphere)
+					//> 139
+					if (myfi.getLeftLightSensor() + myfi.getRightLightSensor()/2 >= left_light + right_light)
 					{
 						System.out.println("Moving Forward");
-						myfi.setLED(0,0,255,10);
-						myfi.setWheelVelocities(150,150);
+						myfi.setLED(0,0,255,50);
+						myfi.setWheelVelocities(myfi.getLeftLightSensor(),myfi.getRightLightSensor());
 						//create new variable & if function for light sensor to keep it max 255
 						//if light sensor detected reach 200, set light sensor value to 255
 						//light = speed
 						//room initial
 						//torch initial
 					}
+					
 					else if (myfi.getRightLightSensor() >= right_light)
 					{
 						System.out.println("Turning Right");
-						myfi.setLED(0,0,255,10);
-						myfi.setWheelVelocities(150,100);
+						myfi.setLED(0,0,255,50);
+						myfi.setWheelVelocities(myfi.getLeftLightSensor()-50,myfi.getRightLightSensor()-100);
 
 					}
 					else if (myfi.getLeftLightSensor()>= left_light)
 					{
 						System.out.println("Turning Left");
-						myfi.setLED(0,0,255,10);
-						myfi.setWheelVelocities(100,150);
+						myfi.setLED(0,0,255,50);
+						myfi.setWheelVelocities(myfi.getLeftLightSensor()-100,myfi.getRightLightSensor()-50);
 
 					}
+					
+					//no light
 					else if (myfi.getLeftLightSensor()<= left_light && myfi.getRightLightSensor() <= right_light)
 					{
 						System.out.println("<NO LIGHT DETECTED!>");
-						myfi.setLED(255,0,0);	
+						myfi.setLED(255,255,0,250);	
 						myfi.stopWheels();
 
-						long beforetime = System.currentTimeMillis(); 											//begins start point for 5 second timer
-						System.out.println("Finch will exit if there is no light for 5 seconds");
-						while(System.currentTimeMillis() - beforetime <= 5000)									//after 5 seconds...the loop will finish, and then proceed to close the application
-						{		
-							if (( (myfi.getLeftLightSensor() > (left_light + 5)) | (myfi.getRightLightSensor() > (right_light + 5 )) )) //added 5 to ambient as light must be a true source, not fluctuations in ambient
-							{	
-								main(args);
-								
-							}
-						}
-						System.out.println("<<<TERMINATED!>>>");
-						System.exit(0);
-					}
-					else 
-					{
-						myfi.stopWheels(); 
-						System.out.println("<<<TERMINATED!>>>");
-						System.exit(0);
+//						long beforetime = System.currentTimeMillis(); 											//begins start point for 5 second timer
+//						System.out.println("Finch will exit if there is no light for 5 seconds");
+//						while(System.currentTimeMillis() - beforetime <= 5000)									//after 5 seconds...the loop will finish, and then proceed to close the application
+//						{		
+//							//detect if light came back
+//							if (( (myfi.getLeftLightSensor() > (left_light + 5)) | (myfi.getRightLightSensor() > (right_light + 5 )) )) //added 5 to ambient as light must be a true source, not fluctuations in ambient
+//							{	
+//								//try not to call yourself i.e main method
+//								main(args);
+//							}
+//						}
+//						System.out.println("<<<TERMINATED!>>>");
+//						System.exit(0);
 					}
 				}
 			}
